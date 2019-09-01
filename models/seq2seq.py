90f6f35cf94b7dffc1474e784c494375d131318c
Daniel Watson <daniel.watson@nyu.edu>
2019-09-01 14:15:52 +0400

    Changed default attention position

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -60,7 +60,7 @@ class Decoder(tfkl.Layer):
         hidden_size,
         num_layers,
         attention="bahdanau",
-        attention_pos=0,
+        attention_pos=2,
         dropout=0.0,
         **kwargs,
     ):
@@ -100,7 +100,7 @@ class Seq2Seq(tfbp.Model):
         "vocab_size": 20000,
         "hidden_size": 256,
         "attention": "bahdanau",  # "bahdanau" or "luong"
-        "attention_pos": 0,
+        "attention_pos": 2,
         "optimizer": "adam",  # "sgd" or "adam"
         "learning_rate": 0.0005,
         "epochs": 5,
